13d39895a8976263661a60aedffd528f6ed0b605
191G0243@rcarbonifera.tecnm.mx <191G0243@rcarbonifera.tecnm.mx>
2023-01-02 20:23:26 -0600

    v2

--- APPMOVIL/APPMOVIL.Android/MainActivity.cs
+++ APPMOVIL/APPMOVIL.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace APPMOVIL.Droid
 {
-    [Activity(Label = "APPMOVIL", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Aerolínea MX", Icon = "@mipmap/Avion", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
